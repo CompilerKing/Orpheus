9003a3e3f9b31b2b6e67f88da46048bacd0dd4b6
dylanhart <dylan96hart@gmail.com>
2015-06-13 14:16:28 -0700

    created title scene

---
+++ Assets/Orpheus/Scripts/LoadLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	public string levelName;
+
+	public void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player")
+		{
+			Application.LoadLevel(levelName);
+		}
+	}
+}
