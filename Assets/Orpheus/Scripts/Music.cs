60488abb771574b29e103e8f413745b4642a5397
dylanhart <dylan96hart@gmail.com>
2015-06-13 22:48:10 -0700

    added jamz

---
+++ Assets/Orpheus/Scripts/Music.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+	
+	private static Music instance = null;
+
+	void Start () {
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad(this);
+		} else if (instance != this) {
+			Destroy(this);
+		}
+	}
+}
